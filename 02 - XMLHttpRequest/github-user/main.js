9f134265af17810c407c811362690657616b3cd5
Sudarshan shinde <shindesudarshan070@gmail.com>
2020-03-30 23:21:24 +0530

    some change

--- 02 - XMLHttpRequest/github-user/main.js
+++ 02 - XMLHttpRequest/github-user/main.js
@@ -4,16 +4,21 @@ let button = document.querySelector("button");
 
 function getData(event) {
   if (input.value != "") {
-    fetch(`https://api.github.com/users/${input.value}`)
-      .then(res => res.json())
-      .then(data => showData(data));
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", `https://api.github.com/users/${input.value}`);
+    xhr.addEventListener("load", () => {
+      showData(JSON.parse(xhr.response));
+    });
+    xhr.send();
+    input.value = "";
   }
 }
 function showData(data) {
   let h2 = document.createElement("h2");
   h2.textContent = `Name: ${data.name}`;
-  let h3 = document.createElement("h3");
-  h3.textContent = `ID: ${data.id}`;
+  //   let h3 = document.createElement("h3");
+  //   h3.textContent = `ID: ${data.id}`;
   let p = document.createElement("p");
   p.textContent = `Bio: ${data.bio}`;
   let repo = document.createElement("p");
@@ -27,7 +32,7 @@ function showData(data) {
   image.classList.add("gitimage");
   img.classList.add("image");
   img.src = data.avatar_url;
-  info.append(h2, h3, p, repo);
+  info.append(h2, p, repo);
   image.append(img);
   flex.append(image, info);
   document.body.append(flex);
